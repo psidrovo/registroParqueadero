8721676235ecaaf09fde63ddd7827ab530fc0e1e
Paul Idrovo <paulsidrovob@gmail.com>
2020-06-22 22:14:58 -0500

    CREACION DE CLASES Se creo las siguientes clases: - controladorCliente - daoCliente - interfaceDaoCliente - mesanje_es_ES - Vehiculo - Ticket - vistaPrincipal - Paquete Iconos

--- src/ec/edu/ups/modelo/Vehiculo.java
+++ src/ec/edu/ups/modelo/Vehiculo.java
@@ -10,5 +10,42 @@ package ec.edu.ups.modelo;
  * @author Paul Idrovo
  */
 public class Vehiculo {
-    
+
+    private String placa;
+    private String marca;
+    private String modelo;
+
+    public Vehiculo() {
+    }
+
+    public Vehiculo(String placa, String marca, String modelo) {
+        this.placa = placa;
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
 }
